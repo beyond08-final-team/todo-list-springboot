c1e4020b58927d5d496a8900c13864d038134d4d
limjihoon99 <bo9701@naver.com>
2024-09-01 20:00:59 +0900

    feat: Frontend 연동을 위한 Config 생성

---
+++ src/main/java/com/beyond/todolist/common/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.beyond.todolist.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 프론트엔드 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
