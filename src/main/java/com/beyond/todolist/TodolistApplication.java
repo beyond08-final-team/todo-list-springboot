55de1a59b50044afcca4f2e8aa346dd002d5333f
hoyeon96 <hoyen96@naver.com>
2024-09-01 22:50:43 +0900

    feat: BaseTimeEntity JpaAuditing 활성화

--- src/main/java/com/beyond/todolist/TodolistApplication.java
+++ src/main/java/com/beyond/todolist/TodolistApplication.java
@@ -2,12 +2,13 @@ package com.beyond.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // JPA Auditing 활성화
 public class TodolistApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TodolistApplication.class, args);
     }
-
 }
