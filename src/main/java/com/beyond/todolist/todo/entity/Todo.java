3f257c4dc098cd8ee20f75e6282ef7b0af8a3099
hoyeon96 <hoyen96@naver.com>
2024-09-01 22:49:20 +0900

    feat: Todo Entity 생성

---
+++ src/main/java/com/beyond/todolist/todo/entity/Todo.java
@@ -0,0 +1,29 @@
+package com.beyond.todolist.todo.entity;
+
+import com.beyond.todolist.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Builder
+public class Todo extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TodoStatus status;
+
+
+
+}
---
+++ src/main/java/com/beyond/todolist/todo/entity/TodoStatus.java
@@ -0,0 +1,6 @@
+package com.beyond.todolist.todo.entity;
+
+public enum TodoStatus {
+    InProgress,
+    Done
+}
