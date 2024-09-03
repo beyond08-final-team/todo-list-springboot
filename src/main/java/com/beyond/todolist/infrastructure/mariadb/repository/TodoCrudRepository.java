74c2e97a3ab2dfa2eb1c3b83a40ce59671657610
yongun2 <djkye0001@naver.com>
2024-09-04 01:31:13 +0900

    update: Todo finaAll abstract method 추가

--- src/main/java/com/beyond/todolist/infrastructure/mariadb/repository/TodoCrudRepository.java
+++ src/main/java/com/beyond/todolist/infrastructure/mariadb/repository/TodoCrudRepository.java
@@ -3,5 +3,9 @@ package com.beyond.todolist.infrastructure.mariadb.repository;
 import com.beyond.todolist.infrastructure.mariadb.entity.TodoEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TodoCrudRepository extends CrudRepository<TodoEntity, Long> {
+
+    List<TodoEntity> findAll();
 }
